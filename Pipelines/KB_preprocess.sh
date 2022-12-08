adb3c8a7c2351293994a76f007fb7f4791f2a95f
didacjs <didacjs@gmail.com>
2022-12-08 11:15:32 +0100

    Set Mouse 1k as default

--- Pipelines/KB_preprocess.sh
+++ Pipelines/KB_preprocess.sh
@@ -7,9 +7,9 @@ source module.sh
 mkdir output
 
 # Set dataset and index path
-data=$"/home/student/Work/Datasets/Human/GSM3295024/"
-t2g=$"/home/student/Work/Refs/Human/T2G/t2g.tsv"
-index=$"/home/student/Work/Refs/Human/index_KB/index_KB"
+data=$"/media/student/Storage/Datasets/1k_mouse_kidney_CNIK_3pv3_fastqs"
+t2g=$"/home/student/Work/Refs/Mouse/T2G/t2g.tsv"
+index=$"/home/student/Work/Refs/Mouse/index_KB/index_KB"
 
 # Which technolgy was used to produce the dataset
 tech="10xv3"
